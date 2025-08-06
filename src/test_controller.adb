90536e148a6592d4441ec0fd149ce9f91d9e0091
david@Paul-Clock <david.haley@bigpond.com>
2025-08-07 00:38:34 +1000

    Updated Test_Controller, provision of new GPIO driver to replace file system driver.

--- src/test_controller.adb
+++ src/test_controller.adb
@@ -1,29 +1,36 @@
+-- Program for testing and calibration ot the hot water pump controller
+-- hardware.
+
 -- Author    : David Haley
 -- Created   : 18/09/2017
--- Last Edit : 13/10/2017
+-- Last Edit : 06/08/2025
+
+-- 20250806 : Sampling rate matched to application rate, to reduce mains
+-- frequency interference.
+-- 20250804 : With and use for rpi_spi removed, spi dependancy limited to
+-- AD7091R2 package. SPI now directly linked to C device driver.
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Calendar; use Ada.Calendar;
 with RPi_GPIO; use RPi_GPIO;
-with RPi_SPI; use RPi_SPI;
 with AD7091R2;
 with RPi_Watchdog;
-with Statistics;
+with DJH.Statistics;
 
 procedure Test_Controller is
 
-   package My_AD is new AD7091R2; use My_AD;
+   package My_AD is new AD7091R2;
+   use My_AD;
+   
    package My_Watchdog is new RPi_Watchdog; use My_Watchdog;
 
    type Int_A_Volts is range A_Volts'First .. A_Volts'Last;
-   package Channel_0_Stats is new Statistics (Int_A_Volts);
-   package Channel_1_Stats is new Statistics (Int_A_Volts);
-
-   Test_Requested : Character;
+   
+   package ADC_Stats is new DJH.Statistics (Int_A_Volts);
+   use ADC_Stats;
 
    Pump_Relay : constant GPIO_Pins := Gen1;
    Fault_LED  : constant GPIO_Pins := Gen2;
-   Test_Reads : Natural := 100000;
 
    task Kick_Watchdog is
       entry Stop_Watchdog;
@@ -46,15 +53,15 @@ procedure Test_Controller is
          or
             delay until Next_Time;
             if Watchdog_State then
-               Kick_Watchdog_Low;
-               Watchdog_State := False;
-            else
                Kick_Watchdog_High;
-               Watchdog_State := True;
+            else
+               Kick_Watchdog_Low;
             end if; -- Watchdog
+            Watchdog_State := not Watchdog_State;
             Next_Time := Next_Time + Kick_Interval;
          end select;
       end loop;  -- not Exit_Now
+      Kick_Watchdog_Low;
    end Kick_Watchdog;
 
    procedure Initialise_Pins is
@@ -64,7 +71,13 @@ procedure Test_Controller is
       Write_Pin (Pin_Low, Pump_Relay);
       Bind_Pin (Fault_LED, Out_Pin);
       Write_Pin (Pin_Low, Fault_LED);
-      Put_Line ("Test Controller 20171013");
+   end Initialise_Pins;
+   
+   function Menu return character is
+   
+	Test_Requested : Character;
+   
+   begin -- Menu
       Put_Line ("0 Exit");
       Put_Line ("1 Turn On Pump Output");
       Put_Line ("2 Turn Off Pump Output");
@@ -73,57 +86,70 @@ procedure Test_Controller is
       Put_Line ("5 Enable Hardware Watchdog");
       Put_Line ("6 Disable Hardware Watchdog");
       Put_Line ("7 Stop Watchdog Task (resets RPi if watchdog is enabled)");
-      Put_Line ("8 Read RAW A/D data" & Natural'Image (Test_Reads) & " Times");
+      Put_Line ("8 Read RAW A/D data");
       Put_Line ("9 Reset A/D Test Statistics");
-   end Initialise_Pins;
-
-   procedure Test_AD is
-
-      AD_Result : A_Volt_Arrays;
-   begin -- Test_AD
-      Put_Line ("Testing A/D Converter");
-      for I in Natural range 1 .. Test_Reads loop
-         AD_Result := AD_Read;
-         Channel_0_Stats.Sample (Int_A_Volts (AD_Result (0)));
-         Channel_1_Stats.Sample (Int_A_Volts (AD_Result (1)));
-      end loop; -- I in Natural range 1 .. Test_Reads
-      Put_Line ("Channel (0) Results");
-      Put_Line ("Test_Reads:" & Natural'Image (Channel_0_Stats.Count));
-      Put_Line ("Mean:" & Channel_0_Stats.My_Float'Image (
-                Channel_0_Stats.Mean));
-      Put_Line ("Minimum:" & Int_A_Volts'Image (Channel_0_Stats.Minimum));
-      Put_Line ("Maximum:" & Int_A_Volts'Image (Channel_0_Stats.Maximum));
-      for I in Channel_0_Stats.Minimum .. Channel_0_Stats.Maximum loop
-         Put_Line ('[' & Int_A_Volts'Image (I) & ']' &
-                     Natural'Image (Channel_0_Stats.Frequency (I)));
-      end loop; -- I in Channel_0_Stats.Minimum .. Channel_0_Stats.Maximum
-      Put_Line ("Variance:" & Channel_0_Stats.My_Float'Image (
-                Channel_0_Stats.Variance));
-      Put_Line ("Channel (1) Results");
-      Put_Line ("Test_Reads:" & Natural'Image (Channel_1_Stats.Count));
-      Put_Line ("Mean:" & Channel_1_Stats.My_Float'Image (
-                Channel_1_Stats.Mean));
-      Put_Line ("Minimum:" & Int_A_Volts'Image (Channel_1_Stats.Minimum));
-      Put_Line ("Maximum:" & Int_A_Volts'Image (Channel_1_Stats.Maximum));
-      for I in Channel_1_Stats.Minimum .. Channel_1_Stats.Maximum loop
-         Put_Line ('[' & Int_A_Volts'Image (I) & ']' &
-                     Natural'Image (Channel_1_Stats.Frequency (I)));
-      end loop; -- I in Channel_1_Stats.Minimum .. Channel_1_Stats.Maximum
-      Put_Line ("Variance:" & Channel_1_Stats.My_Float'Image (
-                Channel_1_Stats.Variance));
-   end Test_AD;
-
-begin -- Test_Controller
-   Initialise_Pins;
-   loop
       Put ("Test? ");
       Get (Test_Requested);
-      case Test_Requested is
+      return Test_Requested;
+   end Menu;
+
+   procedure Test_AD (Channel_0_Stats, Channel_1_Stats : in out Data_Stores) is
+
+      Test_Reads : constant Positive := 100000;
+      Sample_Frequency : constant Positive := 574;
+		-- Highest sample rate orthogonal to harmonics of 48 Hz and 52 Hz
+		Interval : constant Duration := 1.0 / Duration (Sample_Frequency);
+		Next_Time : Time;
+      AD_Result : A_Volt_Arrays;
+      
+   begin -- Test_AD
+      Put_Line ("Testing A/D Converter for" & Test_Reads'Img & " Samples " &
+                "Sample Rate :" & Sample_Frequency'Img & " Test Time :" &
+                Natural'Image (Test_Reads / Sample_Frequency) & " s");
+      Next_Time := Clock;
+      for I in Natural range 1 .. Test_Reads loop
+         AD_Result := AD_Read;
+         Sample (Channel_0_Stats, Int_A_Volts (AD_Result (0)));
+         Sample (Channel_1_Stats, Int_A_Volts (AD_Result (1)));
+         if I mod 2000 = 0 then
+				Put ('.');
+			end if; -- I mod 2000 = 0
+         Next_Time := @ + Interval;
+         Delay until Next_Time;
+      end loop; -- I in Natural range 1 .. Test_Reads
+      New_Line;
+      Put_Line ("Channel (0) ""Tank"" Results");
+      Put_Line ("Mean:" & Mean (Channel_0_Stats)'Img &
+                "  Variance:" & Variance (Channel_0_Stats)'Img);
+      for I in Minimum (Channel_0_Stats) .. Maximum (Channel_0_Stats) loop
+         Put_Line ('[' & I'Img & ']' & Frequency (Channel_0_Stats, I)'Img);
+      end loop; -- in Minimum (Channel_0_Stats) .. Maximum (Channel_0_Stats)
+      Put_Line ("Channel (1) ""Panel"" Results");
+      Put_Line ("Mean:" & Mean (Channel_1_Stats)'Img &
+                "  Variance:" & Variance (Channel_1_Stats)'Img);
+      for I in Minimum( Channel_1_Stats) .. Maximum (Channel_1_Stats) loop
+         Put_Line ('[' & I'Img & ']' & Frequency (Channel_1_Stats, I)'Img);
+      end loop; -- I in Minimum( Channel_1_Stats) .. Maximum (Channel_1_Stats)
+   end Test_AD;
+   
+   Channel_0_Stats, Channel_1_Stats : ADC_Stats.Data_Stores;
+
+begin -- Test_Controller
+   Put_Line ("Test Controller 20250806");
+   Initialise_Pins;
+   loop
+      case Menu is
          when '0' =>
+            Put_Line ("Pump Output Off");
+            Write_Pin (Pin_Low, Pump_Relay);
+            Put_Line ("Fault LED Off");
+            Write_Pin (Pin_Low, Fault_LED);
             Put_Line ("Disabling_Watchdog ready for exit");
             Disable_Watchdog;
-            Put_Line ("Stopping Watchdog task ready for exit");
-            Kick_Watchdog.Stop_Watchdog;
+            if not Kick_Watchdog'Terminated then
+					Put_Line ("Stopping Watchdog task ready for exit");
+					Kick_Watchdog.Stop_Watchdog;
+            end if; -- not Kick_Watchdog'Terminated
             Put_Line ("Ending Tests");
             exit;
          when '1' =>
@@ -148,14 +174,14 @@ begin -- Test_Controller
             Put_Line ("Stop Watchdog task");
             Kick_Watchdog.Stop_Watchdog;
          when '8' =>
-            Test_AD;
+            Test_AD (Channel_0_Stats, Channel_1_Stats);
          when '9' =>
             Put_Line ("Resetting A/D Statistics");
-            Channel_0_Stats.Clear;
-            Channel_1_Stats.Clear;
+            Clear (Channel_0_Stats);
+            Clear (Channel_1_Stats);
          when others =>
             Put_Line ("Invalid Test Request");
-      end case; -- Test_Requested
+      end case; -- Menu
    end loop;
 end Test_Controller;
 
